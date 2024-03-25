0e60451f17e1e354b381216c6a3c472b5b989a71
Evgeny Mangasaryan <mangasaryan.ep@gmail.com>
2024-03-25 18:18:34 +0400

    Ability to toggle on/off signature help

--- lua/plugins/lsp-signature.lua
+++ lua/plugins/lsp-signature.lua
@@ -1,7 +1,13 @@
 return {
     "ray-x/lsp_signature.nvim",
-    event = "InsertEnter",
-    config = function()
-        require("lsp_signature").setup()
+    event = "VeryLazy",
+    config = function(_, opts)
+        local lsp_signature = require("lsp_signature")
+
+        lsp_signature.setup(opts)
+
+        vim.keymap.set({ "n", "i" }, "<C-k>", function()
+            lsp_signature.toggle_float_win()
+        end, { silent = true, noremap = true, desc = "Toggle signature" })
     end
 }
